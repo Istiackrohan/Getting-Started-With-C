7cfced818e6122972745cdd6e890d560163f91a5
Istiackrohan <istiackrohan@gmail.com>
2022-11-25 17:08:39 +0600

    Even Odd Determination is upadted

--- Even-Odd-determination.c
+++ Even-Odd-determination.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 int main (){
-    printf("Hello Brother!");
+    int x;
+    printf("Enter the code you want to check if it is even or odd: ");
+    scanf("%d", &x);
+    x%2==1 ? printf("The number is Odd") : printf("The number is Even");
 return 0;
 }
